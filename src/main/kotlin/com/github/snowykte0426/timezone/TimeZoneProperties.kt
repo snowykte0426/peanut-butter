03fa8193c97320e2bdc652be86e235237fb7c95f
snowykte0426 <snowykte0426@naver.com>
2025-08-30 19:14:16 +0900

    fix: update default timezone in TimeZoneProperties to UTC

--- src/main/kotlin/com/github/snowykte0426/timezone/TimeZoneProperties.kt
+++ src/main/kotlin/com/github/snowykte0426/timezone/TimeZoneProperties.kt
@@ -1,16 +1,15 @@
 package com.github.snowykte0426.timezone
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.bind.ConstructorBinding
 
 /**
  * Configuration properties for timezone settings.
  * Can be configured under `peanut-butter.timezone` in application.yml.
  */
 @ConfigurationProperties(prefix = "peanut-butter.timezone")
-data class TimeZoneProperties @ConstructorBinding constructor(
+data class TimeZoneProperties(
     /**
-     * Timezone to set (default: KST).
+     * Timezone to set (default: UTC).
      * Supported: UTC, KST, JST, GMT, WET, BST, CET, WEST, CEST, EET, EEST, MST, PT, ET
      */
     val zone: String = "UTC",
