14a300e12a981e711f0cd671f32bb3099b1c059e
snowykte0426 <snowykte0426@naver.com>
2025-08-30 12:58:51 +0900

    build: add Jakarta Validation and Hibernate Validator dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,10 @@ repositories {
 }
 
 dependencies {
+    implementation("jakarta.validation:jakarta.validation-api:3.0.2")
+    implementation("org.hibernate.validator:hibernate-validator:8.0.1.Final")
+    implementation("org.glassfish:jakarta.el:4.0.2")
+    
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
