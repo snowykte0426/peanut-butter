e5b1ac7762607744a64351577e51d1a4466ea25c
snowykte0426 <snowykte0426@naver.com>
2025-08-31 08:44:32 +0900

    feat: add Adapter and Port annotations with PortDirection enum

---
+++ src/main/java/com/github/snowykte0426/hexagonal/annotation/PortDirection.java
@@ -0,0 +1,6 @@
+package com.github.snowykte0426.hexagonal.annotation;
+
+public enum PortDirection {
+    INBOUND,
+    OUTBOUND
+}
---
+++ src/main/java/com/github/snowykte0426/hexagonal/annotation/port/Port.java
@@ -0,0 +1,14 @@
+package com.github.snowykte0426.hexagonal.annotation.port;
+
+import com.github.snowykte0426.hexagonal.annotation.PortDirection;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.TYPE})
+public @interface Port {
+    PortDirection direction();
+}
