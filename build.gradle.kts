e8b1fcc9031e6529279c4826e1192c5769ee8aea
snowykte0426 <snowykte0426@naver.com>
2025-08-30 11:29:26 +0900

    build: update project configuration and add publishing setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,18 @@
 plugins {
-    id("java")
+    java
+    `maven-publish`
 }
 
-group = "kr.kro.amond-server"
-version = "1.0-SNAPSHOT"
+group = "com.github.snowykte0426"
+version = "1.0.0"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+
+    withSourcesJar()
+    withJavadocJar()
+}
 
 repositories {
     mavenCentral()
@@ -17,4 +26,44 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            pom {
+                name.set("peanut-butter")
+                description.set("A comprehensive utility library providing essential tools and helper functions for Java development")
+                url.set("https://github.com/snowykte0426/peanut-butter")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("snowykte0426")
+                        name.set("Kim Tae Eun")
+                        email.set("snowykte0426@naver.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/snowykte0426/peanut-butter.git")
+                    developerConnection.set("scm:git:ssh://github.com/snowykte0426/peanut-butter.git") // .git 중복 제거
+                    url.set("https://github.com/snowykte0426/peanut-butter")
+                }
+            }
+        }
+    }
+}
+
+tasks.wrapper {
+    gradleVersion = "8.10"
+    distributionType = Wrapper.DistributionType.ALL
 }
